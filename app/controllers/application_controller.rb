a514d960b19470a2ad0be946a8ec3c9c457d1802
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2022-01-26 16:05:50 +0300

    added post and delete requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   
-  get "/" do
+  get "/colors" do
     colors = Color.all
     colors.to_json(include: :shades)
   end
@@ -12,4 +12,20 @@ class ApplicationController < Sinatra::Base
     shades.to_json
   end
 
+  delete '/shades/:id' do
+    # find the review using the ID
+    shade = Shade.find(params[:id])
+    # delete the review
+    shade.destroy
+    # send a response with the deleted review as JSON
+    shade.to_json
+  end
+
+  post '/shades' do
+    shade = Shade.create(
+      hex_code: params[:hex_code],
+      color_id: params[:color_id]
+    )
+    shade.to_json
+  end
 end
